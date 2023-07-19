a51100c5e45a14bb0a1971c0556db811beb032be
unknown <miraccangursoy1@gmail.com>
2023-07-19 11:30:16 +0300

    bitiyor5

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -38,7 +38,8 @@ function App() {
 ]
 console.log(localStorage.getItem("Challenge11Mode"))
   const[check,setCheck]=useState( localStorage.getItem("Challenge11Mode")==="true" );
-  const[dil,setDil]=useState(localStorage.getItem("challange11Dil")=="Türkçe" || localStorage.getItem("challange11Dil")=="İnglizce"  ? localStorage.getItem("challange11Dil"):"Türkçe"   );
+  console.log(localStorage.getItem("challange11Dil"))
+  const[dil,setDil]=useState( localStorage.getItem("challange11Dil"));
   const [posts, setPosts] = useState(dil=="Türkçe"? arrEn:arrTr)
   
   /*
